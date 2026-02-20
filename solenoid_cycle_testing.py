fb6cec94232a72e9217c991db4bbce263528548d
Andres Flores <dflores.andres@gmail.com>
2026-02-19 22:38:06 -0500

    added continuous load experiment to solenoid cycle testing code

--- solenoid_cycle_testing.py
+++ solenoid_cycle_testing.py
@@ -4,23 +4,47 @@ import time
 
 #  define test parameters
 solenoid_pin = 0  # raspberry pi pico W
-cycles_count = 10
-seconds_per_cycle = 10
+cycles_count = 100
+seconds_per_engage_cycle = 15
+seconds_per_disengage_cycle = 10
+continuous_load_seconds = 30
 
 
+cycle_test=True
 solenoid_manager_class = SolenoidManager(pin=solenoid_pin)
-for engage_cycle in range(cycles_count):
 
 
+if cycle_test:
+    for engage_cycle in range(cycles_count):
+        
+        print(f'CYCLE: {engage_cycle+1}')
+
+        #  hold engage for defined time, seconds
+        solenoid_manager_class.set_engage_flag(flag=True)
+        solenoid_manager_class.drive_solenoid()
+
+        time.sleep(seconds_per_engage_cycle)
+
+
+        #  hold disengage for defined time, seconds
+        solenoid_manager_class.set_engage_flag(flag=False)
+        solenoid_manager_class.drive_solenoid()
+
+        time.sleep(seconds_per_disengage_cycle)
+
+
+else:
+    
+    print(f'TIMED HOLD: {seconds_per_engage_cycle*4} seconds')
+    
+    
     #  hold engage for defined time, seconds
     solenoid_manager_class.set_engage_flag(flag=True)
     solenoid_manager_class.drive_solenoid()
 
-    time.sleep(seconds_per_cycle)
+    time.sleep(continuous_load_seconds)
 
 
     #  hold disengage for defined time, seconds
     solenoid_manager_class.set_engage_flag(flag=False)
     solenoid_manager_class.drive_solenoid()
-
-    time.sleep(seconds_per_cycle)
